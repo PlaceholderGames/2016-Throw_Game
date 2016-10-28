64ff3700dc0ecd04ef0399c795d44ff61537c62d
Davies J (FCES) <16080254@glam.ac.uk>
2016-10-28 15:08:03 +0100

    -Changed can throw mechanic, face left or right and press space bar to spawn car, rather than click left/right of character

--- behavior-hx/SceneEvents_3.hx
+++ behavior-hx/SceneEvents_3.hx
@@ -40,6 +40,7 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2Fixture;
 import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
 
 import motion.Actuate;
 import motion.easing.Back;
@@ -69,16 +70,13 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class ActorEvents_3 extends ActorScript
+class SceneEvents_3 extends SceneScript
 {
-	public var _CansLeft:String;
 	
 	
-	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	public function new(dummy:Int, dummy2:Engine)
 	{
-		super(actor);
-		nameMap.set("CansLeft", "_CansLeft");
-		_CansLeft = "Cans left:";
+		super();
 		
 	}
 	
